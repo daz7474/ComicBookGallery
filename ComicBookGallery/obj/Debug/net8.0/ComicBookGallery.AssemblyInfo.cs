59dfade676ce1d43a8d635013f181e2c408d359b
daz7474 <daz7474@gmail.com>
2024-05-13 16:03:24 +0100

    Updated comic books controller to use repository

--- ComicBookGallery/obj/Debug/net8.0/ComicBookGallery.AssemblyInfo.cs
+++ ComicBookGallery/obj/Debug/net8.0/ComicBookGallery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b1ce5c640200c19f1e96a30f63f0a303083636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5882455b538016a489e3cdeeff450cf4607254c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
