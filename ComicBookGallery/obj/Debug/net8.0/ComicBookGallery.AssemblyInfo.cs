221a2f4fd7baaa94db1a6796cf0060d7a9d736f1
daz7474 <daz7474@gmail.com>
2024-05-13 16:56:15 +0100

    Updated the default route

--- ComicBookGallery/obj/Debug/net8.0/ComicBookGallery.AssemblyInfo.cs
+++ ComicBookGallery/obj/Debug/net8.0/ComicBookGallery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f106594165fbd448c838533b0651245b308c32c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c86034c3bafc869ae06ccd208c53f768c949d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComicBookGallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
